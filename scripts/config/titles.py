c68db822fb94f6278b2e2d20f02f4e8cc1c68aa0
joaozanqui <jpzanqui@hotmail.com>
2025-08-11 18:14:39 -0300

    feat: database - titles

--- scripts/config/titles.py
+++ scripts/config/titles.py
@@ -1,23 +1,19 @@
 import scripts.database as database
 import scripts.utils.handle_text as handle_text
 import scripts.utils.gemini as gemini
+import scripts.utils.inputs as inputs
 import re
 import json
 
-def build_title_prompt(
-    phase1_insights: str,
-    phase2_insights: str,
-    phase3_insights: str,
-    channel: dict,
-) -> str:
-    language = database.get_input_final_language()
+def build_title_prompt(channel: dict, analysis: dict) -> str:
+    language = database.get_item('languages', channel['language_id'])
     json_format_response = f'''[{{"title": "title in {language}", "rationale": "explanation text"}}]'''
-    variables = database.get_variables(channel['id'])
+    variables = database.get_item('channels_responses', channel['id'], column_to_compare='channel_id', select='variables')
 
     other_variables = {
-        "phase1_insights": handle_text.sanitize(phase1_insights),
-        "phase2_insights": handle_text.sanitize(phase2_insights),
-        "phase3_insights": handle_text.sanitize(phase3_insights),
+        "phase1_insights": handle_text.sanitize(analysis['insights_p1']),
+        "phase2_insights": handle_text.sanitize(analysis['insights_p2']),
+        "phase3_insights": handle_text.sanitize(analysis['insights_p3']),
         "channel": handle_text.sanitize(str(channel)),
         "json_format_response": handle_text.sanitize(json_format_response),
         "language": language
@@ -25,17 +21,19 @@ def build_title_prompt(
 
     variables.update(other_variables)
     
-    prompt = database.build_prompt('script', 'titles-generation', variables)
+    prompt = database.get_prompt_template('script', 'titles-generation', variables)
     prompt_json = json.loads(prompt)
-
-    export_path = f"storage/prompts/{channel['id']}/"
-    database.export('titles', prompt_json, format='json',path=export_path)
+    
+    data = {
+        "channel_id": channel['id'],
+        "titles": prompt_json
+    }
+    database.insert(data, 'prompts')
 
     return prompt
 
 def run(channel_id):
-    file_name = 'titles'
-    prompt = database.get_prompt_file(channel_id, file=file_name)
+    prompt = database.get_prompt_file(channel_id, 'titles')
     title_ideas = gemini.run(prompt_json=prompt)
     
     if not title_ideas:
@@ -50,8 +48,25 @@ def run(channel_id):
         return run(channel_id)
 
     print("\nGenerated Viral Video Title Ideas (for your new agent/scripts):")
+    for title in titles_json:
+        print(f"{title['title']} - {title['rationale']}")
 
-    title_ideas_path = database.export(f"{channel_id}", titles_json, format='json',path='storage/ideas/titles/')
-    print(f"Title Ideas saved at {title_ideas_path}")
+    confirm = inputs.yes_or_no("Confirm the titles?")
+
+    if not confirm:
+        return run(channel_id)
+    
+    title_number = database.next_title_number(channel_id)
+    for title in titles_json:
+        data = {
+            "title": title['title'],
+            "rationale": title['rationale'],
+            "channel_id": channel_id,
+            "title_number": title_number
+        }
+        database.insert(data, 'titles')
+        title_number += 1
+    
+    print(f"Title Ideas saved!")
     
     return titles_json
